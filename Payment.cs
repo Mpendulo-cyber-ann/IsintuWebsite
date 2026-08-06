11589fe3b02981bb982a6317bfe95b2e4f4e5624
Mpendulo Nhlakanipho Shelembe <mpenduloshelembe000@gmail.com>
2026-08-06 13:04:40 +0200

    Update Payment.cs

--- Payment.cs
+++ Payment.cs
@@ -6,20 +6,13 @@ namespace IsintuWebsite.Models
     public class Payment
     {
         public int PaymentId { get; set; }
-
         public int OrderId { get; set; }
-
         public virtual Order Order { get; set; }
-
         public DateTime PaymentDate { get; set; }
-
         [Column(TypeName = "decimal")]
         public decimal Amount { get; set; }
-
         public string Method { get; set; } // Credit Card, PayPal, Bank Transfer
-
         public string Status { get; set; } // Pending, Completed, Failed
-
         public Payment()
         {
             PaymentDate = DateTime.Now;
