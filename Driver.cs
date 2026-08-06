22237c00f928efadb8162a60615c02a5a9ad5118
Mpendulo Nhlakanipho Shelembe <mpenduloshelembe000@gmail.com>
2026-08-06 13:05:53 +0200

    Update Driver.cs

--- Driver.cs
+++ Driver.cs
@@ -6,19 +6,13 @@ namespace IsintuWebsite.Models
     public class Driver
     {
         public int DriverId { get; set; }
-
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
-
         public string Phone { get; set; }
-
         public string CarDetails { get; set; }
-
         public string AvailabilityStatus { get; set; } // Available, Unavailable
-
         public virtual ICollection<Delivery> Deliveries { get; set; }
-
         public Driver()
         {
             Deliveries = new List<Delivery>();
